dd0c3b936dd321ad69e9b88a2bfadea2b3d978cb
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-11-03 18:58:14 +0000

    actors i guess

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index"})
 
+  get("/directors/youngest"), { :controller => "directors", :action => "junior"})
+
   get("/directors/:the_id", { :controller => "directors", :action => "show"})
+
+  get("/actors", { :controller => "actors", :action => "index"})
+
+  get("/actors/:the_id", { :controller => "actors", :action => "index"})
 
   get("/movies", { :controller => "movies", :action => "index"})
 

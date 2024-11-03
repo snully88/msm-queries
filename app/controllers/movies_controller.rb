72e4260ece6b2664617b00c16f9d7766a7b025e0
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-11-03 17:58:06 +0000

    confusion

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,10 +5,8 @@ class Movies < ApplicationController
 
   def show
     the_id = params.fetch("the_id")
-
     matching_records = Movie.where({ :id => the_id })
-
     @the_movie = matching_records.at(0)
-    render ({ :template => "movie_templates/details"})
+    render({ :template => "movie_templates/details" })
   end
 end

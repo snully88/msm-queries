3ad6b4dc672c457a41563bca234a0ef6fd379253
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-11-03 16:56:17 +0000

    mmmm

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
-end
+
 
   get("/directors", { :controller => "directors", :action => "index"})
-end
+
   get("/directors/:the_id", { :controller => "directors", :action => "show"})
+  
+end

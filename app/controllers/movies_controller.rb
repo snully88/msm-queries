8aed205b5400fc8921f63d67cc33ebcd7711bac9
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-11-03 18:39:56 +0000

    coolio

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,10 +3,24 @@ class Movies < ApplicationController
     render({ :template => "movie_template/list"})
   end
 
+
   def show
     the_id = params.fetch("the_id")
-    matching_records = Movie.where({ :id => the_id })
-    @the_movie = matching_records.at(0)
-    render({ :template => "movie_templates/details" })
+    matching_records = Movie.where(id: the_id)
+    @the_movie = matching_records.first  # Simplified to use `.first` instead of `.at(0)`
+  
+    if @the_movie.nil?
+      flash[:alert] = "Movie not found."
+      redirect_to movies_path  # Redirect to a suitable path, such as the movies index
+    else
+      render template: "movie_templates/details"
+    end
   end
+
+  #def show
+    #the_id = params.fetch("the_id")
+    #matching_records = Movie.where({ :id => the_id })
+    #@the_movie = matching_records.at(0)
+    #render({ :template => "movie_templates/details" })
+  #end
 end

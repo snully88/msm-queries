3154e49bb32aff6a4edf0bf92369d58b9b45e40f
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-11-03 17:48:10 +0000

    ned to debug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,14 @@
 class Movies < ApplicationController
   def index
     render({ :template => "movie_template/list"})
+  end
+
+  def show
+    the_id = params.fetch("the_id")
+
+    matching_records = Movie.where({ :id => the_id })
+
+    @the_movie = matching_records.at(0)
+    render ({ :template => "movie_templates/details"})
+  end
+end
